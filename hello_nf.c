78c360a9c69c9431b3aaaf304befb87aaaa310d5
cheer.zhang <cheer.zhang@ucloud.cn>
2023-03-03 02:35:15 +0800

    init

---
+++ hello_nf.c
@@ -0,0 +1,105 @@
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/skbuff.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+
+MODULE_LICENSE("GPLv3");
+MODULE_AUTHOR("<cheer.zhang@ucloud.cn>");
+MODULE_DESCRIPTION("Hello Netfliter");
+
+#define NIPQUAD_FMT "%u.%u.%u.%u"
+
+#define HOOK_PARAM_LIST                                     \
+                unsigned int hook, struct sk_buff *skb,     \
+                const struct net_device *in,                \
+                const struct net_device *out,               \
+                int (*okfn)(struct sk_buff*)
+
+static unsigned int hello_pre_routing_hook(HOOK_PARAM_LIST){
+    struct iphdr *ip_header;
+    ip_header = (struct iphdr *)(skb_network_header(skb));
+    printk("@pre_routing: hello '"NIPQUAD_FMT"'\n", ip_header->saddr);
+    return NF_ACCEPT;
+}
+static unsigned int hello_local_in_hook(HOOK_PARAM_LIST){
+    struct iphdr *ip_header;
+    ip_header = (struct iphdr *)(skb_network_header(skb));
+    printk("@local_in: welcome '"NIPQUAD_FMT"'\n", ip_header->saddr);
+    return NF_ACCEPT;
+}
+static unsigned int hello_forward_hook(HOOK_PARAM_LIST){
+    struct iphdr *ip_header;
+    ip_header = (struct iphdr *)(skb_network_header(skb));
+    printk("@forward: hello again '"NIPQUAD_FMT"', and hi'"NIPQUAD_FMT"'.\n",
+        ip_header->saddr, ip_header->daddr);
+    return NF_ACCEPT;
+}
+static unsigned int hello_local_out_hook(HOOK_PARAM_LIST){
+    struct iphdr *ip_header;
+    ip_header = (struct iphdr *)(skb_network_header(skb));
+    printk("@local_out: hi every on, I'm '"NIPQUAD_FMT"'. \n",ip_header->daddr);
+    return NF_ACCEPT;
+}
+static unsigned int hello_post_routing_hook(HOOK_PARAM_LIST){
+    printk("@post_routing: hi '"NIPQUAD_FMT"' . \n",ip_header->saddr);
+    return NF_ACCEPT;
+}
+
+static struct nf_hook_ops hello_nf_ops[] __read_mostly = {
+  {
+    .hook = hello_pre_routing_hook,
+    .pf = NFPROTO_IPV4,
+    .hooknum = NF_INET_PRE_ROUTING,
+    .priority = NF_IP_PRI_FIRST,
+  },{
+    .hook = hello_local_in_hook,
+    .pf = NFPROTO_IPV4,
+    .hooknum = NF_INET_LOCAL_IN,
+    .priority = NF_IP_PRI_FIRST,
+  },
+  {
+    .hook = hello_forward_hook,
+    .pf = NFPROTO_IPV4,
+    .hooknum = NF_INET_FORWARD,
+    .priority = NF_IP_PRI_FIRST,
+  },
+  {
+    .hook = hello_local_out_hook,
+    .pf = NFPROTO_IPV4,
+    .hooknum = NF_INET_LOCAL_OUT,
+    .priority = NF_IP_PRI_FIRST,
+  },
+  {
+    .hook = hello_post_routing_hook,
+    .pf = NFPROTO_IPV4,
+    .hooknum = NF_INET_POST_ROUTING,
+    .priority = NF_IP_PRI_FIRST,
+  }
+};
+
+
+static int __init init_nf_test(void) {
+  int ret;
+  ret = nf_register_hooks(nf_test_ops, ARRAY_SIZE(nf_test_ops));
+  if (ret < 0) {
+    printk("register nf hook fail\n");
+    return ret;
+  }
+  printk(KERN_NOTICE "register nf test hook\n");
+  return 0;
+}
+
+static void __exit exit_nf_test(void) {
+  nf_unregister_hooks(nf_test_ops, ARRAY_SIZE(nf_test_ops));
+}
+
+module_init(init_nf_test);
+module_exit(exit_nf_test);
+
